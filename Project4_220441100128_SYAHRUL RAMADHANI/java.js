5c454b7006bf83e8a25b5d45d45c85afbf8fe6d0
Syahrulrama31 <113223383+Syahrulrama31@users.noreply.github.com>
2023-04-04 20:51:44 +0700

    Add files via upload

---
+++ Project4_220441100128_SYAHRUL RAMADHANI/java.js
@@ -0,0 +1,29 @@
+var scrollToTopBtn = document.querySelector(".scroll-to-top");
+
+window.addEventListener("scroll", function() {
+  // tombol Scroll to Top akan muncul setelah pengguna menggulir ke bawah
+  if (window.pageYOffset > 100) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
+function slider(anything){
+    document.querySelector('.one').src = anything;
+};
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('op en');
+}
